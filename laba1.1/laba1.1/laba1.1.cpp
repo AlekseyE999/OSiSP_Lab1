c9d7ca78d698251a802e480a2e7b63cdd32b8cc0
AlekseyE999 <79412127+AlekseyE999@users.noreply.github.com>
2021-09-09 15:21:37 +0300

    update window

--- laba1.1/laba1.1/laba1.1.cpp
+++ laba1.1/laba1.1/laba1.1.cpp
@@ -1,59 +1,93 @@
 ﻿#include <windows.h>
 #include <stdlib.h>
 
-LRESULT CALLBACK WindProc(HWND hwmd, UINT msg, WPARAM wp, LPARAM lp)
+const wchar_t CLASS_NAME[] = L"MyWindowClass";
+
+
+LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 {
-    switch (msg)
-    {
-    case WM_DESTROY:
-    {
-        PostQuitMessage(0);
-        break;
-    }
-    default:
-        return DefWindowProc(hwmd, msg, wp, lp);
-    }
+	PAINTSTRUCT ps;
+	RECT Rect;
+	HDC hdc, hCmpDC;
+	HBITMAP bitmap;
+	switch (msg)
+	{
+	case WM_PAINT:
+	{
+		GetClientRect(hwnd, &Rect);
+		hdc = BeginPaint(hwnd, &ps);
+		hCmpDC = CreateCompatibleDC(hdc);
+		bitmap = CreateCompatibleBitmap(hdc, Rect.right - Rect.left, Rect.bottom - Rect.top);
+		SelectObject(hCmpDC, bitmap);
+		LOGBRUSH br;
+		br.lbStyle = BS_SOLID;
+		br.lbColor = 0xEECCCC;
+		HBRUSH brush;
+		brush = CreateBrushIndirect(&br);
+		FillRect(hCmpDC, &Rect, brush);
+		DeleteObject(brush);
+		SetStretchBltMode(hdc, COLORONCOLOR);
+		BitBlt(hdc, 0, 0, Rect.right - Rect.left, Rect.bottom - Rect.top, hCmpDC, 0, 0, SRCCOPY);
+		DeleteDC(hCmpDC);
+		DeleteObject(bitmap);
+		hCmpDC = NULL;
+		EndPaint(hwnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+		break;
+	}
+	default:
+		return DefWindowProc(hwnd, msg, wp, lp);
+	}
+	return 0;
 }
 
 int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PWSTR args, int nCmdShow)
 {
-    //MessageBox(NULL, L"HELLO", "meafecd", MB_OK);
-    
-    const wchar_t CLASS_NAME[] = L"MyWindowClass";
-    HWND hwmd;
-    WNDCLASS wc = { };
-    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-    wc.lpfnWndProc = WindProc;
-    wc.hInstance = hInst;
-    wc.lpszClassName = CLASS_NAME;
+	//MessageBox(NULL, L"HELLO", "meafecd", MB_OK);
+
+	HWND hwnd;
+	WNDCLASS wc = { };
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindProc;
+	wc.hInstance = hInst;
+	wc.lpszClassName = CLASS_NAME;
 
 
-    if (!RegisterClass(&wc))
-        return -1;
-    hwmd = CreateWindowEx(0,CLASS_NAME, L"FISICS", WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hInst, NULL);
+	if (!RegisterClass(&wc))
+		return -1;
+	hwnd = CreateWindowEx(0, CLASS_NAME, L"FISICS", WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hInst, NULL);
 
-/*   HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
-        CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
+	/*   HWND hwnd = CreateWindowEx(
+			0,                              // Optional window styles.
+			CLASS_NAME,                     // Window class
+			L"Learn to Program Windows",    // Window text
+			WS_OVERLAPPEDWINDOW,            // Window style
 
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+			// Size and position
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
-        NULL,       // Parent window    
-        NULL,       // Menu
-        hInst,  // Instance handle
-        NULL        // Additional application data
-    );
-*/
-    ShowWindow(hwmd, nCmdShow);
-    MSG msg = { };
-    while (GetMessage(&msg,NULL, NULL, NULL) )
-    {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-    }
-    return 0;
+			NULL,       // Parent window
+			NULL,       // Menu
+			hInst,  // Instance handle
+			NULL        // Additional application data
+		);
+	*/
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
 }
 
